4e8ba27c3bf76ea8e5f7a3f188f0bb56f6ab0537
Avik Chakraborty <92612550+AvikChakraborty1@users.noreply.github.com>
2023-04-08 16:12:09 -0400

    feat: added double jump

--- game.asm
+++ game.asm
@@ -20,6 +20,7 @@
 .data
 all_info:		.space	208	#1-40 space ship, 40-80 fire1 80-120 ufo 120-160 meteor 164 base address 168-208 fire 2
 jump_left:		.byte	15
+can_jump_again:		.byte 	1
 
 .text
 
@@ -59,6 +60,9 @@ game_loop:		li $v0 32
 check_falling:		lw $t6, 256($t0)			# check if pixel below is ground
 			bne $t6, $t8, falling
 			li $t7, 0
+			la $t4, can_jump_again
+			li $t5, 1
+			sb $t5, 0($t4)
 												
 check_keypress:		li $t3 0xffff0000
 			lw $t4 0($t3)
@@ -89,13 +93,21 @@ s_pressed:		sw $t2, 0($t0)
 			sw $t1, 0($t0)
 			j game_loop
 		
-w_pressed:		beq $t7, 1, game_loop
+w_pressed:		beq $t7, 1, try_double_jump
 			beq $t7, 2, game_loop
-			li $t7, 2
+			
+initial_jump:		li $t7, 2
 			sw $t2, 0($t0)
 			addi $t0, $t0, -256
 			sw $t1, 0($t0)
 			j game_loop
+
+try_double_jump:	la $t4, can_jump_again
+			lb $t5, 0($t4)
+			beqz $t5, game_loop
+			addi $t5, $t5, -1
+			sb $t5, 0($t4)
+			j initial_jump
 			
 falling:		li $t7, 1
 			sw $t2, 0($t0)
@@ -119,6 +131,11 @@ at_peak_jump:		li $t7, 1
 			la $t4, jump_left
 			li $t5, MAX_JUMP
 			sb $t5, 0($t4)
+			
+			#la $t4, can_jump_again
+			#lb $t5, 0($t4)
+			#addi $t5, $t5, -1
+			#sb $t5, 0($t4)
 			j check_keypress
 			
 			li $v0, 10
